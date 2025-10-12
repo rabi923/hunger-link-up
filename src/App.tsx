659247755ba218157e9fd5a44c19bd3e11723e22
rabi923 <146437715+rabi923@users.noreply.github.com>
2025-10-12 23:05:20 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ThemeProvider } from "next-themes";
+import { ThemeProvider } from "@/components/src_components_theme-provider_Version2";
 import Index from "./pages/Index";
 import Auth from "./pages/Auth";
 import GiverDashboard from "./pages/GiverDashboard";
